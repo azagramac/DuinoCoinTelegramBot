f1a96e2d11f787bc0ab2115dbe14ef0669dfb9d5
Jose l. Azagra <azagramac@gmail.com>
2021-11-05 11:39:06 +0100

    Update duinocoin.sh

--- duinocoin.sh
+++ duinocoin.sh
@@ -5,13 +5,12 @@ ID="YOUR_CHAT_ID"
 WALLET="YOUR_USERNAME_WALLET"
 URL="https://api.telegram.org/bot$TOKEN/sendMessage"
 MSG="ᕲ DuinoCoin"
+
 JSON=$(curl -s -X GET https://server.duinocoin.com/users/$WALLET -H "Accept: application/json" | jq .)
-#EURO=$(curl -s -X GET http://www.floatrates.com/daily/usd.json -H "Accept: application/json" | jq .eur | jq '.rate')
 BALANCE=$(echo $JSON | jq '.result.balance.balance')
 WORKERS=$(echo $JSON | jq '.result.miners' | jq '.[].identifier')
 NUMBER_WORKERS=$(echo $JSON | jq -r '.result.miners' | jq -r '.[].identifier' | wc -l)
 FORMAT1=$(echo "scale=2; $BALANCE/1" | bc -l)
-#FORMAT2=$(echo "scale=2; $EURO/1" | bc -l)
 
 if [ $? -ne 0 ]
 then
