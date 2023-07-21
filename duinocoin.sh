2b742ceae53cfe7bef1108f0a5bee9d721aee396
Jose <azagramac@gmail.com>
2023-07-21 09:13:23 +0200

    fix format balance output duinocoin.sh

--- duinocoin.sh
+++ duinocoin.sh
@@ -16,13 +16,14 @@ else
     api_telegram="https://api.telegram.org/bot$TOKEN/sendMessage?parse_mode=HTML"
     api_duinocoin=$(curl -s -X GET https://server.duinocoin.com/users/$WALLET -H "Accept: application/json" | jq .)
 
-    BALANCE=$(echo $api_duinocoin | jq '.result.balance.balance' | awk '{printf("%.0f \n",$1)}')
+    BALANCE=$(echo $api_duinocoin | jq '.result.balance.balance')
+    BALANCE_FORMAT=$(echo "scale=2; $BALANCE/1" | bc -l)
     WORKERS=$(echo $api_duinocoin | jq '.result.miners' | jq '.[].identifier' | tr -d '"')
     NUMBER_WORKERS=$(echo $api_duinocoin | jq -r '.result.miners' | jq -r '.[].identifier' | wc -l)
 
     function sendMessage() {
         curl -s -X POST $api_telegram -d chat_id=$CHATID -d text="$(printf "<b>$BANNER</b>\n\n \
-			Balance: $BALANCE\n \
+			Balance: $BALANCE_FORMAT\n \
 			Nº Workers $NUMBER_WORKERS\n \
 			Names workers:\n<code>$WORKERS</code>")" >/dev/null 2>&1
     }
